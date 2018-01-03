4b6cf110907d4a572b95c2592d2f3cec334dc5ad
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-03 23:13:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if roster[grade]
+      roster[grade] << student_name
+    else
+      roster[grade] = []
+      roster[grade] << student_name
+    end
+  end
+
+end
